e0e4351ca25ac1858d71ca9582f5adc45456816b
Ryan Gaus <rsg1egoman@gmail.com>
2026-03-10 13:18:05 -0400

    fix: add isCallerStillConnected to ensure that rpc drops responses for rpc calls that take a long time
    If a RPC call takes a long time and a participant disconnects half way
    through, just drop the return value.

--- src/room/Room.ts
+++ src/room/Room.ts
@@ -1992,6 +1992,7 @@ class Room extends (EventEmitter as new () => TypedEmitter<RoomEventCallbacks>) 
         rpc.compressedPayload,
         rpc.responseTimeoutMs,
         rpc.version,
+        () => this.remoteParticipants.has(packet.participantIdentity),
       );
     }
   };
